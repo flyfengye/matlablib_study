ccfac0bce217e36a55bebce58ed938db9e8749e7
guofei.sun <1316390245@qq.com>
2019-11-06 18:26:47 +0800

    加了几个字  h

--- test1.py
+++ test1.py
@@ -13,7 +13,7 @@ df2 = pd.DataFrame({'A': 1.,
                     'D': np.array([3] * 4, dtype='int32'),
                     'E': pd.Categorical(["test", "train", "test", "train"]),
                     'F': 'foo'})
-# ddd ss
+# ddd ss bb
 # print(df2)
 # print(df2.dtypes)
 # df2.<Tab>
